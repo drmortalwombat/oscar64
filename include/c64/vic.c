ea2b59e64d11a2698d04c7b8fc805259c7d78187
drmortalwombat <90205530+drmortalwombat@users.noreply.github.com>
2021-11-28 15:17:39 +0100

    Fix sprite index in vic.c

--- include/c64/vic.c
+++ include/c64/vic.c
@@ -8,8 +8,8 @@ void vic_setbank(char bank)
 
 void vic_sprxy(byte s, int x, int y)
 {
-	vic.spr_pos[0].y = y;
-	vic.spr_pos[0].x = x & 0xff;
+	vic.spr_pos[s].y = y;
+	vic.spr_pos[s].x = x & 0xff;
 	if (x & 0x100)
 		vic.spr_msbx |= 1 << s;
 	else
