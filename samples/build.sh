a0ef0b3be27e869b56597b4ccc151a122d4ad863
Janne Johansson <janne.johansson@safespring.com>
2023-10-24 21:02:51 +0200

    Simplify samples/build.sh by using subshells

--- samples/build.sh
+++ samples/build.sh
@@ -1,45 +1,24 @@
 #!/bin/sh
-cd fractals
-./build.sh
-cd ..
 
-cd games
-./build.sh
-cd ..
+( cd fractals ; ./build.sh )
 
-cd hires
-./build.sh
-cd ..
+( cd games ; ./build.sh )
 
-cd hiresmc
-./build.sh
-cd ..
+( cd hires ; ./build.sh )
 
-cd particles
-./build.sh
-cd ..
+( cd hiresmc ; ./build.sh )
 
-cd kernalio
-./build.sh
-cd ..
+( cd particles ; ./build.sh )
 
-cd memmap
-./build.sh
-cd ..
+( cd kernalio ; ./build.sh )
 
-cd rasterirq
-./build.sh
-cd ..
+( cd memmap ; ./build.sh )
 
-cd scrolling
-./build.sh
-cd ..
+( cd rasterirq ; ./build.sh )
 
-cd sprites
-./build.sh
-cd ..
+( cd scrolling ; ./build.sh )
 
-cd stdio
-./build.sh
-cd ..
+( cd sprites ; ./build.sh )
+
+( cd stdio ; ./build.sh )
 
