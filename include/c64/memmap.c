bfe4e618aab03cab47f980a6b51c314d200494f3
drmortalwombat <90205530+drmortalwombat@users.noreply.github.com>
2025-08-13 21:36:34 +0200

    fix memmap.c comments

--- include/c64/memmap.c
+++ include/c64/memmap.c
@@ -9,13 +9,13 @@
 
 __asm DoneTrampoline
 {
-	stx $01				; The ROM code at jmp ($fffa) has saved our X value 
-						; so we can restore it to $01. Our banks our back to whatever it was
-	pla					; now we pull X and A and restore them
+	stx $01				// The ROM code at jmp ($fffa) has saved our X value 
+						//  so we can restore it to $01. Our banks our back to whatever it was
+	pla					//  now we pull X and A and restore them
 	tax
 	pla
-	rti					; RTI can now pull the original status byte and return address and 
-						; return to the original code.
+	rti					//  RTI can now pull the original status byte and return address and 
+						//  return to the original code.
 }
 
 __asm IRQTrampoline
@@ -39,29 +39,29 @@ __asm IRQTrampoline
 
 __asm NMITrampoline
 {
-							; NMI just happend, so stack contains ($01ff for example):
-							; $01ff High byte of return address
-							; $01fe Low byte of return address
-							; $01fd status flag byte
+							//  NMI just happend, so stack contains ($01ff for example):
+							//  $01ff High byte of return address
+							//  $01fe Low byte of return address
+							//  $01fd status flag byte
 
-	pha						; $01fc save A on the stack
+	pha						//  $01fc save A on the stack
 	txa
-	pha						; $01fb save X on the stack
+	pha						//  $01fb save X on the stack
 
-	lda #>DoneTrampoline	; $01fa save the high byte of DoneTrampoline()
+	lda #>DoneTrampoline	//  $01fa save the high byte of DoneTrampoline()
 	pha
-	lda #<DoneTrampoline	; $01f9 save the low byte of DoneTrampoline()
+	lda #<DoneTrampoline	//  $01f9 save the low byte of DoneTrampoline()
 	pha
-	tsx						; transfer the SP ($f8) to X
-	lda $0105, x			; $0105 + $f8 = $01fd (we have virtually shifted the end of stack
-							; 			 to $0105 to get to the original status flag)
-	pha						; and we push it again
-	ldx $01					; Now we save the current $01 value so we can restore it later
-	lda #$36				; set $01 to its default value (bank ROMs back in)
+	tsx						//  transfer the SP ($f8) to X
+	lda $0105, x			//  $0105 + $f8 = $01fd (we have virtually shifted the end of stack
+							//  			 to $0105 to get to the original status flag)
+	pha						//  and we push it again
+	ldx $01					//  Now we save the current $01 value so we can restore it later
+	lda #$36				//  set $01 to its default value (bank ROMs back in)
 	sta $01
-	jmp ($fffa)				; call the original handler (we are looking at ROM now, not RAM)
-							; this routine saves A, X and Y and ends in an RTI 
-							; that will pop SP and the DoneTrampoline() address and jump to it
+	jmp ($fffa)				//  call the original handler (we are looking at ROM now, not RAM)
+							//  this routine saves A, X and Y and ends in an RTI 
+							//  that will pop SP and the DoneTrampoline() address and jump to it
 }
 
 void mmap_trampoline(void)
