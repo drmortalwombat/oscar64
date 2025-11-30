42860b6f3d6912b6e1b2a31be447c82ba9a387f6
drmortalwombat <90205530+drmortalwombat@users.noreply.github.com>
2025-11-30 15:51:24 +0100

    Bump version number

--- oscar64/oscar64.cpp
+++ oscar64/oscar64.cpp
@@ -81,7 +81,7 @@ int main2(int argc, const char** argv)
 
 #else
 		strcpy(strProductName, "oscar64");
-		strcpy(strProductVersion, "1.32.265");
+		strcpy(strProductVersion, "1.32.266");
 
 #ifdef __APPLE__
 		uint32_t length = sizeof(basePath);
